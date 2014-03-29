37f1be41d126de78db87ceb52e64787abe8479fc
Ryon Coleman <ryon55@gmail.com>
2014-03-28 22:35:02 -0400

    Add some polish to app views/templates

--- app/view/Event.js
+++ app/view/Event.js
@@ -14,27 +14,76 @@ Ext.define('PTWMobile2014.view.Event', {
 		// pressable config
 		pressedTarget: '.event-links a',
 
-		title: 'Event',
+		title: 'Details',
+		cls: 'event-details',
 		scrollable: true,
-		styleHtmlContent: true,
 		styleHtmlContent: true,
 		itemId: 'EventInfoPanel',
 		tpl: [
-			'<div class="eventInfo">',
-			 '	<h1 class="title">{Title}</h1>', 
-			 '	<tpl if="Address"><p class="address">{Address}</p></tpl>', 
-			 '	<p class="dates">', 
-			 	'<time>{[Ext.DateExtras.getShortMonthName(values.StartTime.getMonth())]} {[values.StartTime.getDate()]} - {[Ext.DateExtras.getShortMonthName(values.EndTime.getMonth())]} {[values.EndTime.getDate()]}, {[values.StartTime.getFullYear()]}</time>', '<br>', '<time>{StartDate:date("g:i A")} to {EndTime:date("g:i A")}</time>', '	</p>', 
-			 	'	<tpl if="Description"><div class="description"><p>{[values.Description.replace(/[\\r\\n]+/g, \'</p><p>\')]}</p></div></tpl>', 
-			 	'	<ul class="event-links">', 
-			 	'		<tpl if="Address"    ><li><a href="http://maps.google.com/?q={Address}" target="_system" class="wtf">Directions</a></li></tpl>',
-			 	'		<tpl if="RegisterURL"><li><a href="{RegisterURL}" target="_blank">Register</a></li></tpl>',
-			 	'		<tpl if="TwitterURL" ><li><a href="{TwitterURL}"  target="_system">Twitter</a></li></tpl>', 
-			 	'		<tpl if="FacebookURL"><li><a href="{FacebookURL}" target="_blank">Facebook</a></li></tpl>', 
-			 	'		<tpl if="WebsiteURL" ><li><a href="{WebsiteURL}"  target="_blank">Website</a></li></tpl>', 
-			 	'	</ul>', 
-		 	'</div>'
+		    '<header class="event-header"',
+                '<tpl if="EventPhotoID"> style="background-image:url(http://phillytechweek.com/thumbnail/{EventPhotoID}/600x600)"</tpl>',
+                '>',
+    
+                '<div class="event-header-info">',
+                    '<div class="event-date">',
+                        '{[Ext.Date.format(values.StartTime, "l, F j")]}',
+                    '</div>',
+                    '<h1 class="event-title">{Title}</h1>',
+                '</div>',    
+            '</header>',
+            
+            '<div class="event-datetime">',
+            
+                '<div class="event-time">',
+                    '{[this.getShortTime(values.StartTime)]}',
+                    '<tpl if="StartTime != EndTime">',
+                        '&ndash;',
+                        '{[this.getShortTime(values.EndTime)]}',
+                    '</tpl>',
+                '</div>',
+            
+            '</div>',
+
+            '<div class="event-location">',
+                '<tpl if="LocationName">{LocationName}</tpl>',
+                '<tpl if="LocationName && LocationAddress">, </tpl>',
+                '<tpl if="LocationAddress">{LocationAddress}</tpl>',
+            '</div>',
+            
+            '<div class="event-description">{Description}</div>',
+            
+            {
+                getShortTime: function(time) {
+                    var hour = parseInt(Ext.Date.format(time, 'g')),
+                        min  = parseInt(Ext.Date.format(time, 'i')),
+                        ampm = Ext.Date.format(time, 'a');
+
+                    if (min > 9) {
+                        min = ':' + min;
+                    } else if (min > 0) {
+                        min = ':0' + min;
+                    }
+
+                    return hour + min + ampm;
+                }
+            }
 		]
+/*
+		tpl: [
+            '	<h1 class="title">{Title}</h1>', 
+            '	<tpl if="Address"><p class="address">{Address}</p></tpl>', 
+            '	<p class="dates">', 
+            	'<time>{[Ext.DateExtras.getShortMonthName(values.StartTime.getMonth())]} {[values.StartTime.getDate()]} - {[Ext.DateExtras.getShortMonthName(values.EndTime.getMonth())]} {[values.EndTime.getDate()]}, {[values.StartTime.getFullYear()]}</time>', '<br>', '<time>{StartDate:date("g:i A")} to {EndTime:date("g:i A")}</time>', '	</p>', 
+            	'	<tpl if="Description"><div class="description"><p>{[values.Description.replace(/[\\r\\n]+/g, \'</p><p>\')]}</p></div></tpl>', 
+            	'	<ul class="event-links">', 
+            	'		<tpl if="Address"    ><li><a href="http://maps.google.com/?q={Address}" target="_system" class="wtf">Directions</a></li></tpl>',
+            	'		<tpl if="RegisterURL"><li><a href="{RegisterURL}" target="_blank">Register</a></li></tpl>',
+            	'		<tpl if="TwitterURL" ><li><a href="{TwitterURL}"  target="_system">Twitter</a></li></tpl>', 
+            	'		<tpl if="FacebookURL"><li><a href="{FacebookURL}" target="_blank">Facebook</a></li></tpl>', 
+            	'		<tpl if="WebsiteURL" ><li><a href="{WebsiteURL}"  target="_blank">Website</a></li></tpl>', 
+            	'	</ul>', 
+		]
+*/
 	},
 
 	updateEvent: function(newEvent, oldEvent) {
--- app/view/Main.js
+++ app/view/Main.js
@@ -7,18 +7,47 @@ Ext.define('PTWMobile2014.view.Main', {
 
     config: {
 
+        layout: {
+            animation: {
+                duration: 200
+            }
+        },
         items: [{
-            title: 'Schedule',
+            title: '#ptw14 Schedule',
+            cls: 'schedule-list',
 
             xtype: 'list',
             store: 'Schedule',
             grouped: true,
             itemTpl: [
-                '<div class="calendar">', 
-                    '<h1 class="event-title">{Title}</h1>', 
-                    '<p>{Address}</p>', 
-                    '<time><span class="day">{[Ext.Date.format(values.StartTime, "D, M j")]}, </span>{[Ext.Date.format(values.StartTime, "g:i A")]} to {[Ext.Date.format(values.EndTime, "g:i A")]}</time>',
-                '</div>'
+                '<div class="event-time">',
+                    '{[this.getShortTime(values.StartTime)]}',
+                    '<tpl if="StartTime != EndTime">',
+                        '&ndash;',
+                        '{[this.getShortTime(values.EndTime)]}',
+                    '</tpl>',
+                '</div>',
+                '<h1 class="event-title">{Title}</h1>', 
+                '<div class="event-location">',
+                    '<tpl if="LocationName">{LocationName}</tpl>',
+                    '<tpl if="LocationName && LocationAddress">, </tpl>',
+                    '<tpl if="LocationAddress">{LocationAddress}</tpl>',
+                '</div>',
+                {
+                    getShortTime: function(time) {
+                        var hour = parseInt(Ext.Date.format(time, 'g')),
+                            min  = parseInt(Ext.Date.format(time, 'i')),
+                            ampm = Ext.Date.format(time, 'a');
+
+                        if (min > 9) {
+                            min = ':' + min;
+                        } else if (min > 0) {
+                            min = ':0' + min;
+                        }
+
+                        return hour + min + ampm;
+                    }
+                }
             ]
         }]
     }
